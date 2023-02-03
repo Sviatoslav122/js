dbc76e12ff05ec8e018fffbee4e8086b8ea79a08
Sviatoslav122 <72312547+Sviatoslav122@users.noreply.github.com>
2023-02-03 11:52:39 +0200

    homework js 2

--- task2/main.js
+++ task2/main.js
@@ -223,4 +223,40 @@ if (num > num2){
 //         за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
 //
 //
+// let x1 = prompt();
+// switch (x1){
+
 //     - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+
+    let coursesAndDurationArray = [
+        {title: 'JavaScript Complex', monthDuration: 5},
+        {title: 'Java Complex', monthDuration: 6},
+        {title: 'Python Complex', monthDuration: 6},
+        {title: 'QA Complex', monthDuration: 4},
+        {title: 'FullStack', monthDuration: 7},
+        {title: 'Frontend', monthDuration: 4}
+    ]
+let oursesAndDurationArray0 = coursesAndDurationArray[0];
+if (oursesAndDurationArray0.monthDuration >= 5){
+    console.log('super')
+}
+let oursesAndDurationArray1 = coursesAndDurationArray[1];
+if (oursesAndDurationArray1.monthDuration >= 5){
+    console.log('super')
+}
+let oursesAndDurationArray2 = coursesAndDurationArray[2];
+if (oursesAndDurationArray2.monthDuration >= 5){
+    console.log('super')
+}
+let oursesAndDurationArray3 = coursesAndDurationArray[3];
+if (oursesAndDurationArray3.monthDuration >= 5){
+    console.log('super')
+}
+let oursesAndDurationArray4 = coursesAndDurationArray[4];
+if (oursesAndDurationArray4.monthDuration >= 5){
+    console.log('super')
+}
+let oursesAndDurationArray5 = coursesAndDurationArray[5];
+if (oursesAndDurationArray5.monthDuration >= 5){
+    console.log('super')
+}
